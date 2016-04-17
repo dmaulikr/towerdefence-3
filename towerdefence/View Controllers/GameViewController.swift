65b56bac58b8c06e8069d2f3ea95aeafecebceb7
Simon Andersson <simon@hiddencode.me>
2016-04-17 23:10:14 +0200

    Removed fps and shizzle

--- towerdefence/View Controllers/GameViewController.swift
+++ towerdefence/View Controllers/GameViewController.swift
@@ -15,9 +15,6 @@ class GameViewController: UIViewController {
 		super.viewDidLoad()
 
 		let skView = self.view as! SKView
-		skView.showsFPS = true
-		skView.showsNodeCount = true
-		
 		/* Sprite Kit applies additional optimizations to improve rendering performance */
 		skView.ignoresSiblingOrder = true
 		
